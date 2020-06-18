ce07fe3bcd6bd51fce712f69f77add0fba2b31f0
Nomad-Ryan <uneed.in@gmail.com>
2020-06-18 23:22:24 +0900

    fix

--- init/js/script.js
+++ init/js/script.js
@@ -4,20 +4,33 @@ $(function () {
      * ui script initialize
      */
 
+    portfolio.init();
+
 })
 
 
-const portfolio ={
+const portfolio = {
     init: function () {
-
+        portfolio.getScroll();
     },
 
     getScroll: function () {
-        $(window).on('scroll',function (
+        $(window).on('scroll', function () {
+            const scrollTop = $(window).scrollTop();
+            console.log("sct", scrollTop);
+            const offsetTopWorks = $('.works').offset().top;
 
-        ) {
+            if(scrollTop > 400) {
+                $("html").addClass("show-bio");
+            } else {
+                $("html").removeClass("show-bio");
+            }
 
-        } )
-
+            if(scrollTop > offsetTopWorks - 150) {
+                $('#header').fadeIn();
+            } else {
+                $('#header').fadeOut();
+            }
+        })
     }
 }
