d0476b4c0889a7bebc0c38f64f95f5d26c6239e1
Nomad-Ryan <uneed.in@gmail.com>
2020-06-18 22:28:19 +0900

    init

---
+++ init/js/script.js
@@ -0,0 +1,23 @@
+$(function () {
+
+    /**
+     * ui script initialize
+     */
+
+})
+
+
+const portfolio ={
+    init: function () {
+
+    },
+
+    getScroll: function () {
+        $(window).on('scroll',function (
+
+        ) {
+
+        } )
+
+    }
+}
